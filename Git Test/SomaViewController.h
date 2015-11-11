ca201f2c8ff943c3ac15f3ecdcd3074023713867
haithamTory <haitham99@ymail.com>
2015-11-11 09:25:30 +0200

    add soma classes

---
+++ Git Test/SomaViewController.h
@@ -0,0 +1,13 @@
+//
+//  SomaViewController.h
+//  Git Test
+//
+//  Created by PiTechnologies on 11/11/15.
+//  Copyright (c) 2015 PiTechnologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SomaViewController : UIViewController
+
+@end
---
+++ Git Test/SomaViewController.m
@@ -0,0 +1,38 @@
+//
+//  SomaViewController.m
+//  Git Test
+//
+//  Created by PiTechnologies on 11/11/15.
+//  Copyright (c) 2015 PiTechnologies. All rights reserved.
+//
+
+#import "SomaViewController.h"
+
+@interface SomaViewController ()
+
+@end
+
+@implementation SomaViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
